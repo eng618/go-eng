028ac47cb5d5388c4b89c22928355c6253867f73
Eric N. Garcia <eng618@garciaericn.com>
2022-04-05 12:14:56 -0400

    feat: stub circular package

---
+++ algo/circular/slice.go
@@ -0,0 +1,9 @@
+package circular
+
+import "fmt"
+
+// mod should be size of array.
+//
+func Hello() {
+	fmt.Println("circular say's hi!!")
+}
---
+++ algo/circular/slice_test.go
@@ -0,0 +1,25 @@
+package circular_test
+
+import (
+	"testing"
+
+	"github.com/eng618/go-eng/algo/circular"
+)
+
+func TestHello(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name string
+	}{
+		{name: "Temp Test"},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			circular.Hello()
+		})
+	}
+}
