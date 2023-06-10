6b00b549cf3e1a31d6a5906565835c4313bd8a38
Eric N. Garcia <eng618@garciaericn.com>
2023-06-10 01:51:05 -0400

    feat(examples): add RESTFul API using Gin

---
+++ examples/web-service-gin/doc.go
@@ -0,0 +1,14 @@
+/*
+package main is an example of how to write a web service using gin.
+
+This example follows the tutorial: https://go.dev/doc/tutorial/web-service-gin
+
+To test this code locally, you must clone the repository, `cd` into it, and run the following:
+
+```shell
+go run ./examples/web-service-gin
+```
+
+The above command will run the server locally and you can interact easily via `curl` or other methods.
+*/
+package main
