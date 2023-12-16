c7cdf03dd3346000d57c4e5c1ccc68feead76846
Eric N. Garcia <eng618@garciaericn.com>
2023-12-16 11:57:42 -0500

    docs(grammar): updates phonetics

--- ds/std/doc.go
+++ ds/std/doc.go
@@ -1,5 +1,5 @@
 /*
-Package std is used to demonstrates a data structures that are defined in the
+Package std is used to demonstrate data structures that are defined in the
 standard library.
 
 - container/heap
