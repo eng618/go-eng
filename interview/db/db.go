b5720259bf9f8430f2c22453b4378de1e501ca73
Eric N. Garcia <eng618@garciaericn.com>
2023-12-18 15:23:26 -0500

    feat(db): add in memory database example

---
+++ interview/db/db.go
@@ -0,0 +1,71 @@
+package db
+
+import (
+	"fmt"
+	"time"
+)
+
+// InMemDB is a simple in memory database.
+type InMemDB struct {
+	data map[string]map[time.Time]interface{}
+}
+
+// NewDatabase instantiates a new instance of a database.
+func NewDatabase() *InMemDB {
+	return &InMemDB{
+		data: make(map[string]map[time.Time]interface{}),
+	}
+}
+
+// Set creates a new entry into an existing database.
+func (db *InMemDB) Set(key string, value interface{}) {
+	t := time.Now()
+	if v, ok := db.data[key]; ok {
+		// Here we need to update v
+		v[t] = value
+		return
+	}
+
+	newVal := make(map[time.Time]interface{})
+	newVal[t] = value
+
+	db.data[key] = newVal
+}
+
+// Get attempts to retrieve the supplied key from an existing database.
+func (db *InMemDB) Get(key string) interface{} {
+	if v, ok := db.data[key]; ok {
+		var ot time.Time
+		var finalValue interface{}
+		for t, val := range v {
+			if t.After(ot) {
+				ot = t
+				finalValue = val
+			}
+		}
+
+		return finalValue
+	}
+
+	fmt.Println("Value is not in database")
+	return nil
+}
+
+// Get attempts to retrieve the supplied key from an existing database.
+func (db *InMemDB) GetForTime(key string, t time.Time) (interface{}, bool) {
+	if v, ok := db.data[key]; ok {
+		if finalValue, ok := v[t]; ok {
+			return finalValue, true
+		}
+	}
+
+	fmt.Println("No value for:", key, "at:", t, "found in database.")
+	return nil, false
+}
+
+// Print is a helper method to print each entry in a database to it's own line.
+func (db *InMemDB) Print() {
+	for k, v := range db.data {
+		fmt.Printf("%v: %v\n", k, v)
+	}
+}
---
+++ interview/db/db_test.go
@@ -0,0 +1,32 @@
+package db_test
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/eng618/go-eng/interview/db"
+)
+
+func Example() {
+	db := db.NewDatabase()
+
+	db.Set("foo", "bar")
+	fmt.Println("just set foo to:", db.Get("foo"))
+
+	db.Set("foo", "baz")
+	fmt.Println("the latest foo is:", db.Get("foo"))
+
+	// DB can set ints also
+	db.Set("age", 36)
+	age := db.Get("age")
+	fmt.Println("the age in database is:", age)
+
+	// You can use the common ok idiom to get a key with a particular time stamp.
+	if val, ok := db.GetForTime("foo", time.Now()); ok {
+		fmt.Println(val)
+	}
+
+	db.Print()
+
+	// Output:
+}
