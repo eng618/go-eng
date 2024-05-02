92b896795096f7df922879776ebeb8a2c5f981ea
Eric N. Garcia <eng618@garciaericn.com>
2024-05-02 00:16:44 -0400

    build/ci: various adjustments

--- ds/list/linkedlist_test.go
+++ ds/list/linkedlist_test.go
@@ -92,7 +92,7 @@ func TestLinkedList_Delete(t *testing.T) {
 	}
 
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
+		t.Run(tt.name, func(_ *testing.T) {
 			_ = ll.Delete(tt.args.position)
 		})
 	}
